a04a69c1fdee3bfb6ea2c5b8d5c5b497e4aa5d67
abedevjs <bilarasa@gmail.com>
2023-06-04 14:24:00 +0800

    modal and 404 re-design

--- js/modal.js
+++ js/modal.js
@@ -15,6 +15,29 @@ const close = function() {
     modal.classList.add('hidden');
 }
 
-const text = [window, btnClose];
-
 btnClose.addEventListener('click', close);
+
+////////////////////////////////////////////////////////////////////////////
+
+const authBtns__container = document.querySelector('.authBtns__container');
+const auth__buttons = document.querySelectorAll('.auth__buttons');
+const auth__contents = document.querySelectorAll('.auth__contents');
+
+authBtns__container.addEventListener('click', (e) => {
+
+    const clicked = e.target.closest('.auth__buttons');
+
+    if (!clicked) return;
+
+    //Remove active class on Button
+    auth__buttons.forEach(btn => btn.classList.remove('auth__buttons--active'));
+
+    //Remove active class on Content
+    auth__contents.forEach(btn => btn.classList.remove('auth__contents--active'));
+
+    //Add active class on Button
+    clicked.classList.add('auth__buttons--active');
+
+     //Activate Auth Content
+     document.querySelector(`.auth__contents--${clicked.dataset.tab}`).classList.add('auth__contents--active');
+})
